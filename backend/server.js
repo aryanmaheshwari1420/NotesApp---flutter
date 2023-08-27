2a08c42611f0c658636c02fb1f2bcc6f51545f95
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-08-27 17:24:28 +0530

    done with the connection with mongoose and mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 5000; // You can change this to another port if needed
 
+const mongoose = require('mongoose');
+// until mongoose not connect server will not start
+mongoose.connect("mongodb+srv://aryanmaheshwari1420:rr1234@cluster0.ehf7z7f.mongodb.net/notesdb").then(function(){
 
 //Home Route ex - "/"
 app.get("/", function (req, res) {
@@ -11,6 +13,13 @@ app.get("/", function (req, res) {
 app.get("/notes", function (req, res) {
     res.send("This is the Notes Page");
  });
+
+});
+
+
+
+
+const port = 5000; 
 
 app.listen(port, () => {
    console.log(`Server is running on port ${port}`);
