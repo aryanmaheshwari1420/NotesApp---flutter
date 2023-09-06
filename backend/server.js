20f560bd214f00205ce868e60a2f4cbed9887b19
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-09-06 20:24:03 +0530

    small change

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const mongoose = require("mongoose");
 const Note = require("./src/models/Note");
 const MongoUrl =
   "mongodb+srv://aryanmaheshwari1420:rr1234@cluster0.ehf7z7f.mongodb.net/notesdb";
-mongoose.connect(process.env.MONGO_URI).then(function () {
+mongoose.connect(MongoUrl).then(function () {
   //Home Route ex - "/"
   app.get("/", function (req, res) {
     const response = { message: "API Works!" };
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI).then(function () {
   app.use("/notes", noteRouter);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
