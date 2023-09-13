b8ee4015687c80e4a2afb2cb7681ca5d5744f39b
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-09-13 19:32:23 +0530

    small change

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,7 +23,7 @@ mongoose.connect(MongoUrl).then(function () {
   app.use("/notes", noteRouter);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
   
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
