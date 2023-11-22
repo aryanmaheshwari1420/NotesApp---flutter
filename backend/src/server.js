e01acbb7bbbfd7baa87a15f6e20cbd2c4de69c80
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-11-23 00:39:06 +0530

    done

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const mongoDbPath = "mongodb+srv://rohitsemriwal:rr1234@cluster0.fnizk.mongodb.net/notesdb";
+const mongoDbPath = "mongodb+srv://aryanmaheshwari1420:rr1234@cluster0.ehf7z7f.mongodb.net/notesdb";
 mongoose.connect(mongoDbPath).then(function() {
     app.get("/", function(req, res) {
         const response = { statuscode: res.statusCode, message: "API Works!" };
